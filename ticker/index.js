db4425d0aab99023db94e5391f255662b7e22917
Joao Santos <joaomiguelopesantos@hotmail.com>
2020-05-12 16:51:16 +0200

    ticker exercise part 2 plus bonus challenge

--- ticker/index.js
+++ ticker/index.js
@@ -1,18 +1,53 @@
 (function () {
-    var headlines = document.getElementById("headlines");
-    var links = headlines.getElementsByTagName("a");
+    var headlinesTopLeft = document.getElementById("headlines-top-left");
+    var headlinesBottomRight = document.getElementById(
+        "headlines-bottom-right"
+    );
+    var linksTop = headlinesTopLeft.getElementsByTagName("a");
+    var linksBottom = headlinesBottomRight.getElementsByTagName("a");
 
-    var left = headlines.offsetLeft;
+    var left = headlinesTopLeft.offsetLeft;
+    var right = headlinesBottomRight.offsetLeft - window.innerWidth;
 
-    moveElement();
+    var animationIdLeft;
+    var animationIdRight;
 
-    function moveElement() {
-        if (left <= -links[0].offsetWidth) {
-            left += links[0].offsetWidth;
-            links[0].parentNode.appendChild(links[0]);
+    moveElementToTheLeft();
+    moveElementToTheRight();
+
+    headlinesTopLeft.addEventListener("mouseover", function () {
+        cancelAnimationFrame(animationIdLeft);
+    });
+
+    headlinesBottomRight.addEventListener("mouseover", function () {
+        cancelAnimationFrame(animationIdRight);
+    });
+
+    headlinesTopLeft.addEventListener("mouseout", function () {
+        moveElementToTheLeft();
+    });
+
+    headlinesBottomRight.addEventListener("mouseout", function () {
+        moveElementToTheRight();
+    });
+
+    function moveElementToTheLeft() {
+        if (left <= -linksTop[0].offsetWidth) {
+            left += linksTop[0].offsetWidth;
+            linksTop[0].parentNode.appendChild(linksTop[0]);
         }
         left--;
-        headlines.style.left = left + "px";
-        requestAnimationFrame(moveElement);
+        headlinesTopLeft.style.left = left + "px";
+        animationIdLeft = requestAnimationFrame(moveElementToTheLeft);
+    }
+
+    function moveElementToTheRight() {
+        if (right >= -linksBottom[0].offsetWidth) {
+            right -= linksBottom[0].offsetWidth;
+            linksBottom[0].parentNode.appendChild(linksBottom[0]);
+        }
+        right++;
+        headlinesBottomRight.style.left = right + "px";
+        animationIdRight = requestAnimationFrame(moveElementToTheRight);
     }
 })();
