2a7d1bac62089484f36457fc2aafe82ca648e6d3
Joao Santos <joaomiguelopesantos@hotmail.com>
2020-05-25 17:27:26 +0200

    local storage exercise

---
+++ local_storage/index.js
@@ -0,0 +1,18 @@
+// Make a static HTML page that has a large <textarea> on it.
+// When the user types in it, save the value in localStorage.
+// When the user comes back to the page after navigating away or
+// closing the browser, the stored value should automatically appear in the <textarea>.
+
+var textarea = $("textarea");
+
+textarea.on("input", function () {
+    try {
+        localStorage.setItem("input", this.value);
+    } catch (err) {
+        console.log("Oh nooooo");
+    }
+});
+
+$(document).ready(function () {
+    textarea.val(localStorage.getItem("input"));
+});
