00228e3da203296b0588e4c8941af633d83c05ca
Joao Santos <joaomiguelopesantos@hotmail.com>
2020-05-26 14:12:24 +0200

    ajax exercise refactor

--- jquery/ticker/index.js
+++ jquery/ticker/index.js
@@ -1,52 +1,68 @@
-(function () {
-    var headlinesTopLeft = $("#headlines-top-left");
-    var headlinesBottomRight = $("#headlines-bottom-right");
-    var linksTop = headlinesTopLeft.find("a");
-    var linksBottom = headlinesBottomRight.find("a");
+var headlinesTopLeft = $("#headlines-top-left");
+var headlinesBottomRight = $("#headlines-bottom-right");
+var linksTop = headlinesTopLeft.find("a");
+var linksBottom = headlinesBottomRight.find("a");
+var left;
+var right;
+var animationIdLeft;
+var animationIdRight;
 
-    var left = headlinesTopLeft.offset().left;
-    var right = headlinesBottomRight.offset().left - window.innerWidth;
+headlinesTopLeft.on("mouseover", function () {
+    cancelAnimationFrame(animationIdLeft);
+});
 
-    var animationIdLeft;
-    var animationIdRight;
+headlinesBottomRight.on("mouseover", function () {
+    cancelAnimationFrame(animationIdRight);
+});
 
+headlinesTopLeft.on("mouseout", function () {
     moveElementToTheLeft();
+});
+
+headlinesBottomRight.on("mouseout", function () {
     moveElementToTheRight();
+});
 
-    headlinesTopLeft.on("mouseover", function () {
-        cancelAnimationFrame(animationIdLeft);
-    });
+function moveElementToTheLeft() {
+    console.log("moving left");
 
-    headlinesBottomRight.on("mouseover", function () {
-        cancelAnimationFrame(animationIdRight);
-    });
+    if (left <= -linksTop.eq(0).outerWidth()) {
+        left += linksTop.eq(0).outerWidth();
+        linksTop.eq(0).appendTo(linksTop);
+    }
+    left--;
+    headlinesTopLeft.css({ left: left + "px" });
+    animationIdLeft = requestAnimationFrame(moveElementToTheLeft);
+}
 
-    headlinesTopLeft.on("mouseout", function () {
+function moveElementToTheRight() {
+    console.log("moving right");
+    if (right >= -linksBottom.eq(0).outerWidth()) {
+        right -= linksBottom.eq(0).outerWidth();
+        linksBottom.eq(0).appendTo(linksBottom);
+    }
+
+    right++;
+    headlinesBottomRight.css({ left: right + "px" });
+    animationIdRight = requestAnimationFrame(moveElementToTheRight);
+}
+
+$(document).ready(
+    $.get("/data.json", function (data) {
+        console.log("data", data);
+        for (var i = 0; i < 8; i++) {
+            if (i < 4) {
+                linksTop.eq(i).attr("href", data[i].url);
+                linksTop.eq(i).text(data[i].text);
+            } else {
+                linksBottom.eq(i - 4).attr("href", data[i].url);
+                linksBottom.eq(i - 4).text(data[i].text);
+            }
+        }
+        left = headlinesTopLeft.offset().left;
+        right = headlinesBottomRight.offset().left - window.innerWidth;
+
         moveElementToTheLeft();
-    });
-
-    headlinesBottomRight.on("mouseout", function () {
         moveElementToTheRight();
-    });
-
-    function moveElementToTheLeft() {
-        if (left <= -linksTop.eq(0).outerWidth()) {
-            left += linksTop.eq(0).outerWidth();
-            linksTop.eq(0).appendTo(linksTop);
-        }
-        left--;
-        headlinesTopLeft.css({ left: left + "px" });
-        animationIdLeft = requestAnimationFrame(moveElementToTheLeft);
-    }
-
-    function moveElementToTheRight() {
-        if (right >= -linksBottom.eq(0).outerWidth()) {
-            right -= linksBottom.eq(0).outerWidth();
-            linksBottom.eq(0).appendTo(linksBottom);
-        }
-
-        right++;
-        headlinesBottomRight.css({ left: right + "px" });
-        animationIdRight = requestAnimationFrame(moveElementToTheRight);
-    }
-})();
+    })
+);
