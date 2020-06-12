664a559242c872ac5d3738cf51de9bb4ba7e3c19
Joao Santos <joaomiguelopesantos@hotmail.com>
2020-06-12 13:47:11 +0200

    fs refactor promise

--- node/fs/index.js
+++ node/fs/index.js
@@ -1,29 +1,45 @@
 const fs = require("fs");
+const { readdir, stat } = require("fs").promises;
 
 // Part 1
-
-function logSizes(fullPath) {
-    fs.readdir(fullPath, { withFileTypes: true }, (err, files) => {
-        if (err) {
-            return console.error(err);
-        }
-
-        for (let file of files) {
-            fs.stat(`${fullPath}/${file.name}`, (err, fileStats) => {
-                if (err) {
-                    return console.error(err);
-                }
-                if (file.isFile()) {
-                    console.log(`${fullPath}/${file.name}: ${fileStats.size}`);
-                } else if (file.isDirectory()) {
-                    logSizes(`${fullPath}/${file.name}`);
-                }
-            });
-        }
+function recursiveAll(array) {
+    // Wait for the promises to resolve
+    return Promise.all(array).then(function (result) {
+        // If no new promises were added, return the result
+        if (result.length == array.length) return result;
+        // If new promises were added, re-evaluate the array.
+        return recursiveAll(array);
     });
 }
 
-logSizes(__dirname);
+const logSizes = (fullPath) =>
+    new Promise((resolve, reject) => {
+        let promises = [];
+        readdir(fullPath, { withFileTypes: true })
+            .then((files) => {
+                for (let file of files) {
+                    promises.push(
+                        stat(`${fullPath}/${file.name}`)
+                            .then((fileStats) => {
+                                if (file.isFile()) {
+                                    console.log(
+                                        `${fullPath}/${file.name}: ${fileStats.size}`
+                                    );
+                                } else if (file.isDirectory()) {
+                                    return logSizes(`${fullPath}/${file.name}`);
+                                }
+                            })
+                            .catch((err) => console.log(err))
+                    );
+                }
+                resolve(Promise.all(promises));
+            })
+            .catch((err) => reject(console.log(err)));
+    });
+
+logSizes(__dirname)
+    .then(() => console.log("done!"))
+    .catch(() => console.log("oops something went wrong"));
 
 // Part 2
 
